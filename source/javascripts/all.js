f6cf8e026b35c587b1c84e5d56e5603b8e744e42
CHH <chh@chh.tw>
2013-07-28 22:13:01 +0800

    用 directive 處理 tag 的顯示方式。

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,12 +1,39 @@
 //= require_tree .
 
-angular.module('chh', ['chh.controllers']);
+angular.module('chh', ['chh.controllers', 'chh.directives', 'chh.factories']);
 
+/* Controllers */
 angular.module('chh.controllers', []).
 
-controller('stuffCtrl', function($scope) {
+controller('stuffCtrl', function($scope, stuff) {
   $scope.filters = {};
-  $scope.stuffes = [
+  $scope.stuffes = stuff;
+})
+
+/* Directives */
+angular.module('chh.directives', []).
+
+directive('tagsFormatter', function($compile) {
+  return {
+    scope: {
+      tags: '='
+    },
+    restrict: 'C',
+    link: function(scope, element, attrs) {
+      var template ='<ul><li ng-repeat="tag in tags"><a ng-href="#{{tag}}">{{tag}}</a></li></ul>';
+      var newElement = angular.element(template);
+      $compile(newElement)(scope);
+      element.append(newElement);
+    }
+  }
+})
+
+/* Factories */
+angular.module('chh.factories', []).
+
+factory('stuff', function() {
+  return [
+    // 圖片尺寸 470 x 380
     {
       name: 'Twinkle',
       id: 'twinkle',
@@ -27,7 +54,7 @@ controller('stuffCtrl', function($scope) {
       name: "Sublime Text 手冊",
       id: "docs-sublime-text",
       url: "http://docs.sublimetext.tw",
-      images: [ "docs-sublimetext-tw-1.png", "docs-sublimetext-tw-2.png", "docs-sublimetext-tw-3.png" ],
+      images: ["docs-sublimetext-tw-1.png", "docs-sublimetext-tw-2.png", "docs-sublimetext-tw-3.png"],
       description: "2013 年最熱門的程式編輯器 Sublime Text 的中文手冊。",
       tags: ['html5', 'compass']
     },
@@ -35,11 +62,9 @@ controller('stuffCtrl', function($scope) {
       name: "Dropbot Theme",
       id: "dropbot-theme",
       url: "http://blog.chh.tw",
-      images: [ "blog-chh-tw-1.png", "blog-chh-tw-2.png", "blog-chh-tw-3.png" ],
+      images: ["blog-chh-tw-1.png", "blog-chh-tw-2.png", "blog-chh-tw-3.png"],
       description: "一套 Octopress 的佈景主題，更適合中文排版，以及更專注於內容。",
-      tags: ['html5', 'scss', 'compass']
+      tags: ['html5', 'compass']
     }
   ]
 })
-
-// 圖片尺寸 470 x 380
